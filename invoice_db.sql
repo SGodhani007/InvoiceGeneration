fbd51997c3c91dccdcf5a9505e84e4bedc4117a6
SGodhani007 <141123515+SGodhani007@users.noreply.github.com>
2024-07-29 23:52:30 +0530

    Add files via upload

---
+++ invoice_db.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 29, 2024 at 08:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `invoice_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `SID` int(11) NOT NULL,
+  `INVOICE_NO` int(11) NOT NULL,
+  `INVOICE_DATE` date NOT NULL,
+  `CNAME` varchar(50) NOT NULL,
+  `CADDRESS` varchar(150) NOT NULL,
+  `C_CITY` varchar(50) NOT NULL,
+  `GRAND_TOTAL` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `invoice`
+--
+
+INSERT INTO `invoice` (`SID`, `INVOICE_NO`, `INVOICE_DATE`, `CNAME`, `CADDRESS`, `C_CITY`, `GRAND_TOTAL`) VALUES
+(1, 1, '2024-07-07', 'Sahil Godhani', '61, Prabhudarshan Row House, Shyamdham Chowk, Nana Varachha', 'Surat', 9585000),
+(2, 2, '2024-07-08', 'Sanket Keshwani', 'Radha Sarees', 'Varanasi', 225000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_products`
+--
+
+CREATE TABLE `invoice_products` (
+  `ID` int(11) NOT NULL,
+  `SID` int(11) NOT NULL,
+  `PNAME` varchar(50) NOT NULL,
+  `PRICE` double NOT NULL,
+  `QTY` int(11) NOT NULL,
+  `TOTAL` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `invoice_products`
+--
+
+INSERT INTO `invoice_products` (`ID`, `SID`, `PNAME`, `PRICE`, `QTY`, `TOTAL`) VALUES
+(1, 1, 'M416', 90000, 100, 9000000),
+(2, 1, 'UMP5', 4500, 100, 450000),
+(3, 1, 'Ammo 5.56', 90, 1000, 90000),
+(4, 1, 'Ammo 45.ACP', 45, 1000, 45000),
+(5, 2, 'Lotus 365', 9000, 25, 225000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`SID`);
+
+--
+-- Indexes for table `invoice_products`
+--
+ALTER TABLE `invoice_products`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `SID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `invoice_products`
+--
+ALTER TABLE `invoice_products`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
